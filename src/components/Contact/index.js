1d4c7521a1434a777c8f0862dadfb77e4aef7929
Amir <amir.hackett@gmail.com>
2022-07-02 12:12:54 -0400

    added validation to email and name values

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,30 @@
 import React, { useState } from "react";
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name:'', email: '', message: ''});
     const { name, email, message } = formState;
+    const [errorMessage, serErrorMessage] = useState('');
 
     function handleChange(e) {
-        setFormState({...formState, name: e.target.value })
+        if(e.target.name === 'email'){
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+
+            if(!isValid){
+                serErrorMessage('Your email is invalid');
+            } else {
+                serErrorMessage('');
+            } 
+        } else {
+            if (!e.target.value.length) {
+              setErrorMessage(`${e.target.name} is required.`);
+            } else {
+              setErrorMessage('');
+            }
+        } if (!errorMessage) {
+            setFormState({...formState, name: e.target.value })
+        }
     };
 
     function handleSubmit(e){
